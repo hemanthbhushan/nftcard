4455e9cf12a8d423356236a7b128db11f278ef47
Hemanth Bhushan <hemanth.bhushan@antiersolutions.com>
2023-02-14 10:33:46 +0530

    commit message

--- src/components/GetTokens.js
+++ src/components/GetTokens.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './GetTokens.css'
 import { ethers, BigNumber } from "ethers";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import icon from '../images/icon-ethereum.svg';
-import ButtonCard from './ButtonCard'
+import ButtonCard from './ButtonCard';
+import {
+  Ethereum,
+  DAI,
+  USDT
+} from '../constants/Address/addressStore.js'
 
 const GetTokens = () => {
-
+  const [formToken, setFormToken] = useState(null)
 
   const formik = useFormik({
     initialValues: {
       fromToken: "",
+      fromAmount:"",
       toToken: "",
-      amountIn: "",
-      toAddress:"",
+      toAmount:""
     },
     validationSchema: Yup.object({
-        addressToken1: Yup.string().oneOf(["0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6","0x73967c6a0904aa032c103b4104747e88c566b1a2","0x509ee0d083ddf8ac028f2a56731412edd63223b9"]).required("Required"),
-        addressToken2: Yup.string().oneOf(["0xBE9ac76dB19b0135D493fa8fFfD7b6093d871432"]).required("Required"),
-        amountIn: Yup.string().required("Required"),
-        toAddress: Yup.string().required("Required"),
+      fromToken: Yup.string().oneOf([Ethereum,DAI,USDT]).required("Required"),
+      fromAmount: Yup.string().required("Required"),
+       toToken: Yup.string().required("Required"),
     }),
     onSubmit:  async (values) => {
 
@@ -32,27 +36,40 @@ const GetTokens = () => {
   return (
     
     <div>
-       <div>
-        <main className="container">
+    <form  onSubmit={formik.handleSubmit}>
+    <main className="container">
     <section className="main-card">
-    <h1  className="title">From</h1>
-    <span class="custom-dropdown big image-container">
-     <select> 
-            <option>Select One</option>   
-            <option>Ethereum</option>
-            <option>DAI</option>  
-            <option>USDC</option>
+    <h1 className="title">Swap</h1>
+    <h2  className="title">From</h2>
+    <span className="custom-dropdown big image-container">
+     <select 
+      id='fromToken'
+      name='fromToken'
+      placeholder='Select One'
+      type = 'text'
+      {...formik.getFieldProps("fromToken")}
+        > 
+            <option value = "">Select One</option>   
+            <option value={Ethereum}>Ethereum</option>
+            <option value={DAI}>DAI</option>  
+            <option value={USDT}>USDT</option>
             
         </select>
-    </span>
-      <div className="text-container">
-      <h1 className="title">To</h1>
-        <div className="eth-info">
-          <div className="info">
-            <img src={icon}alt="ETH" className="icon"/><span className="eth">pricee</span>
+     </span>
+    <span className="far fa-user">
+          <div class="form__group">
+            <input type="text" className="form__input" id="fromAmount" placeholder="Enter Amount" {...formik.getFieldProps("fromAmount")} />
+            {/* <label for="fromAmount" className="form__label">Enter Amount</label> */}
+            {formik.touched.fromAmount  ? (
+           <div >{formik.errors.fromAmount}</div>
+            ) : null}
           </div>
-          
-        </div>
+            
+      </span>
+   
+      <div className="text-container">
+      <h1 className="title">YOU GET</h1>
+       
         <div className="creator-info">
          
          <ButtonCard/>
@@ -60,7 +77,8 @@ const GetTokens = () => {
       </div>
     </section>
   </main>
-</div>
+  </form> 
+
  
 </div>
   )
--- src/constants/Address/addressStore.js
+++ src/constants/Address/addressStore.js
@@ -1,1 +1,4 @@
 export const closedSea = "0xc3B6B0E93d2bFd66b611b55cB9d76d605Bf1bf7a";
+export const Ethereum = "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6";
+export const DAI = "0x73967c6a0904aa032c103b4104747e88c566b1a2";
+export const USDT = "0x509ee0d083ddf8ac028f2a56731412edd63223b9";
